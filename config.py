aa4be7d628696f457cba9611a3ca161600e7f6cb
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2025-07-07 17:20:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "24072444"
+API_ID = "14050586"
 # -------------------------------------------------------------
-API_HASH = "580e1a3e7d0719816feebf595c9166d2"
+API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
-SUPPORT_GRP = "Ur_Support07"
-UPDATE_CHNL = "Ur_Rishu_143"
-OWNER_USERNAME = "RishuCoder"
+OWNER_ID = int(getenv("OWNER_ID", "6068463116"))
+SUPPORT_GRP = "synaxchatgroup"
+UPDATE_CHNL = "synaxnetwork"
+OWNER_USERNAME = "coder_s4nax"
     
