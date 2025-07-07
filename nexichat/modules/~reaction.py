bacef7fed1513218d80d64de464bbae59ec85d3c
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2025-07-07 17:40:17 +0530

    Update ~reaction.py

--- nexichat/modules/~reaction.py
+++ nexichat/modules/~reaction.py
@@ -5,6 +5,6 @@ from nexichat import nexichat
 @nexichat.on_message(filters.incoming)
 async def react_to_messages(client: Client, message: Message):
     try:
-        await message.react("👍")
+        await message.react("❤️")
     except Exception as e:
         print(f"Failed to react to message: {e}")
