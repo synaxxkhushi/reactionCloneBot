c661d947bc3c301b63f2012a2b8f1b391eb9ec07
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2025-07-07 17:15:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "24072444"
+# -------------------------------------------------------------
+API_HASH = "580e1a3e7d0719816feebf595c9166d2"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
+SUPPORT_GRP = "Ur_Support07"
+UPDATE_CHNL = "Ur_Rishu_143"
+OWNER_USERNAME = "RishuCoder"
+    
